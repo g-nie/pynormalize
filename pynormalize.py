1bb27edc2a9c93d735f86a7205b2197f9259e017
Giannis Terzopoulos <terzo.giannis@gmail.com>
2018-03-05 17:21:44 +0200

    Added main function

--- pynormalize.py
+++ pynormalize.py
@@ -24,64 +24,69 @@ def valid_files(arg):
         raise argparse.ArgumentTypeError(msg)
 
 
-start = datetime.now()
+def main():
+    start = datetime.now()
 
-if not os.path.exists(DIRECTORY):  # Create the directory if it doesn't exist
-    os.mkdir(DIRECTORY)
+    if not os.path.exists(DIRECTORY):  # Create the directory if it doesn't exist
+        os.mkdir(DIRECTORY)
 
-# Accept only one of the arguments : either --files or --all
-parser = argparse.ArgumentParser(description='Normalize audio files to a specified level of dBs (supports .mp3 and .flac)')
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-f', '--files', help='Audio files to normalize.', type=valid_files, nargs='+')
-group.add_argument('-a', '--all', help='Process all the audio files in current directory.', action='store_true')
-args = parser.parse_args()
+    # Accept only one of the arguments : either --files or --all
+    parser = argparse.ArgumentParser(description='Normalize audio files to a specified level of dBs (supports .mp3 and .flac)')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-f', '--files', help='Audio files to normalize.', type=valid_files, nargs='+')
+    group.add_argument('-a', '--all', help='Process all the audio files in current directory.', action='store_true')
+    args = parser.parse_args()
 
-if args.all:  # User selected --all
-    Audio = []
-    for f in os.listdir():
-        if f.endswith('.mp3') or f.endswith('.flac'):
-            Audio.append(f)
-    if not Audio:
-        print('Current directory has no audio files.')
-        sys.exit(1)
-else:  # User selected --files
-    Audio = args.files
+    if args.all:  # User selected --all
+        Audio = []
+        for f in os.listdir():
+            if f.endswith('.mp3') or f.endswith('.flac'):
+                Audio.append(f)
+        if not Audio:
+            print('Current directory has no audio files.')
+            sys.exit(1)
+    else:  # User selected --files
+        Audio = args.files
 
-for file in Audio:
-    if os.sep in file:
-        filename = file[file.rfind(os.sep) + 1:]
-    else:
-        filename = file
-    extension = file[file.rfind('.') + 1:]
-    # Extract metadata information
-    if extension == 'mp3':
-        form = 'mp3'
-        title = MP3(file).tags.get('TIT2', '')
-        artist = MP3(file).tags.get('TPE1', '')
-        album = MP3(file).tags.get('TALB', '')
-        tracknumber = MP3(file).tags.get('TRCK', '')
-    else:
-        form = 'flac'
-        title = FLAC(file).get('title')[0] if FLAC(file).get('title') else ''
-        artist = FLAC(file).get('artist')[0] if FLAC(file).get('artist') else ''
-        album = FLAC(file).get('album')[0] if FLAC(file).get('album') else ''
-        tracknumber = FLAC(file).get('tracknumber')[0] if FLAC(file).get('tracknumber') else ''
-    # Export the edited file
-    song = AudioSegment.from_file(file, format=form)
-    change_in_dBFS = TARGET_DBFS - song.dBFS
-    dest_file = os.path.join(DIRECTORY, filename)
-    normalized_sound = song.apply_gain(change_in_dBFS)
-    normalized_sound.export(dest_file, format=form)
+    for file in Audio:
+        if os.sep in file:
+            filename = file[file.rfind(os.sep) + 1:]
+        else:
+            filename = file
+        extension = file[file.rfind('.') + 1:]
+        # Extract metadata information
+        if extension == 'mp3':
+            form = 'mp3'
+            title = MP3(file).tags.get('TIT2', '')
+            artist = MP3(file).tags.get('TPE1', '')
+            album = MP3(file).tags.get('TALB', '')
+            tracknumber = MP3(file).tags.get('TRCK', '')
+        else:
+            form = 'flac'
+            title = FLAC(file).get('title')[0] if FLAC(file).get('title') else ''
+            artist = FLAC(file).get('artist')[0] if FLAC(file).get('artist') else ''
+            album = FLAC(file).get('album')[0] if FLAC(file).get('album') else ''
+            tracknumber = FLAC(file).get('tracknumber')[0] if FLAC(file).get('tracknumber') else ''
+        # Export the edited file
+        song = AudioSegment.from_file(file, format=form)
+        change_in_dBFS = TARGET_DBFS - song.dBFS
+        dest_file = os.path.join(DIRECTORY, filename)
+        normalized_sound = song.apply_gain(change_in_dBFS)
+        normalized_sound.export(dest_file, format=form)
 
-    # Write metadata in new file
-    if extension == 'mp3':
-        audio = MP3(dest_file)
-    else:
-        audio = FLAC(dest_file)
-    audio["title"] = title
-    audio["artist"] = artist
-    audio["album"] = album
-    audio["tracknumber"] = tracknumber
-    audio.save()
+        # Write metadata in new file
+        if extension == 'mp3':
+            audio = MP3(dest_file)
+        else:
+            audio = FLAC(dest_file)
+        audio["title"] = title
+        audio["artist"] = artist
+        audio["album"] = album
+        audio["tracknumber"] = tracknumber
+        audio.save()
 
-print('Execution time : {} seconds'.format((datetime.now() - start).seconds))
+    print('Execution time : {} seconds'.format((datetime.now() - start).seconds))
+
+
+if __name__ == '__main__':
+    main()
