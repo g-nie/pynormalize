c39c36bdf1827e40f96b46bdb9af195f0eb83cc8
Giannis Terzopoulos <terzo.giannis@gmail.com>
2018-03-06 17:53:57 +0200

    Refactored and added more audio formats

--- pynormalize/pynormalize.py
+++ pynormalize/pynormalize.py
@@ -3,30 +3,25 @@ import os
 import sys
 import warnings
 from datetime import datetime
-from mutagen.mp3 import MP3
-from mutagen.flac import FLAC
+import mutagen
 with warnings.catch_warnings():
     # Ignore RuntimeWarning about absence of ffmpeg
     warnings.simplefilter("ignore")
     from pydub import AudioSegment
 
 
-# TODO : handle .wav and other formats
+# TODO : CHECK WHAT HAPPENS WHEN FFMPEG NOT PRESENT
 
 DIRECTORY = '_NORMALIZED'  # Directory to store edited files
 
 
-# Check if given files exist in the system and are supported
+# Check if given files exist in the file system
 def valid_files(arg):
-    if os.path.isfile(arg):
-        if arg.endswith('.mp3') or arg.endswith('.flac'):
-            return arg
-        else:
-            msg = 'File type is not supported : {}'.format(arg)
-            raise argparse.ArgumentTypeError(msg)
-    else:
+    if not os.path.isfile(arg):
         msg = 'File cannot be found : {}'.format(arg)
         raise argparse.ArgumentTypeError(msg)
+    else:
+        return arg
 
 
 start = datetime.now()
@@ -34,7 +29,8 @@ start = datetime.now()
 if not os.path.exists(DIRECTORY):  # Create the directory if it doesn't exist
     os.mkdir(DIRECTORY)
 
-parser = argparse.ArgumentParser(description='Normalize audio files to a specified level of dBs (supports .mp3 and .flac)')
+parser = argparse.ArgumentParser(description="""Normalize audio files to a specified level of dBs
+    (supports .mp3 and .flac) \nSupported audio formats : .wav, .flac, .mp3, .ogg, .webm, .mp4""")
 parser.add_argument('-d', '--decibels', help='Decibels to use in normalization', type=float, default='-13.5')
 # Accept only one of these arguments : either --files or --all
 group = parser.add_mutually_exclusive_group(required=True)
@@ -45,8 +41,11 @@ args = parser.parse_args()
 target_dbfs = args.decibels
 if args.all:  # User selected --all
     Audio = []
+    Supported = ('.wav', '.flac', '.mp3', '.ogg', '.webm', '.mp4')
+    # TODO : SUPPORT MORE FORMATS (ERROR IMPORTING .WMA - ERROR EXPORTING .M4A, .APE, .AAC)
+
     for f in os.listdir():
-        if f.endswith('.mp3') or f.endswith('.flac'):
+        if f.endswith(Supported):  # +++++++++++++
             Audio.append(f)
     if not Audio:
         print('Current directory has no audio files.')
@@ -55,40 +54,23 @@ else:  # User selected --files
     Audio = args.files
 
 for file in Audio:
-    if os.sep in file:
-        filename = file[file.rfind(os.sep) + 1:]
-    else:
-        filename = file
-    extension = file[file.rfind('.') + 1:]
-    # Extract metadata information
-    if extension == 'mp3':
-        form = 'mp3'
-        title = MP3(file).tags.get('TIT2', '')
-        artist = MP3(file).tags.get('TPE1', '')
-        album = MP3(file).tags.get('TALB', '')
-        tracknumber = MP3(file).tags.get('TRCK', '')
-    else:
-        form = 'flac'
-        title = FLAC(file).get('title')[0] if FLAC(file).get('title') else ''
-        artist = FLAC(file).get('artist')[0] if FLAC(file).get('artist') else ''
-        album = FLAC(file).get('album')[0] if FLAC(file).get('album') else ''
-        tracknumber = FLAC(file).get('tracknumber')[0] if FLAC(file).get('tracknumber') else ''
+
+    (dirname, filename) = os.path.split(file)
+    (shortname, extension) = os.path.splitext(filename)
+
+    tags = mutagen.File(file, easy=True)  # Extract metadata information
+
     # Export the edited file
-    song = AudioSegment.from_file(file, format=form)
+    song = AudioSegment.from_file(file, format=extension[1:])
     change_in_dBFS = target_dbfs - song.dBFS
     dest_file = os.path.join(DIRECTORY, filename)
     normalized_sound = song.apply_gain(change_in_dBFS)
-    normalized_sound.export(dest_file, format=form)
+    bitrate = '320k' if 'mp3' in extension else None
+    normalized_sound.export(dest_file, format=extension[1:], bitrate=bitrate)
 
-    # Write metadata in new file
-    if extension == 'mp3':
-        audio = MP3(dest_file)
-    else:
-        audio = FLAC(dest_file)
-    audio["title"] = title
-    audio["artist"] = artist
-    audio["album"] = album
-    audio["tracknumber"] = tracknumber
-    audio.save()
+    # Copy metadata to new file (if the original has metadata)
+    if tags:
+        tags.save(dest_file)
+
 
 print('Execution time : {} seconds'.format((datetime.now() - start).seconds))
