e153cd0f342fe83309c6e7671affe095d86cd139
Giannis Terzopoulos <terzo.giannis@gmail.com>
2018-03-06 18:32:55 +0200

    Refactored

--- pynormalize/pynormalize.py
+++ pynormalize/pynormalize.py
@@ -5,15 +5,20 @@ import warnings
 from datetime import datetime
 import mutagen
 with warnings.catch_warnings():
-    # Ignore RuntimeWarning about absence of ffmpeg
+    # Silence RuntimeWarning about absence of ffmpeg
     warnings.simplefilter("ignore")
     from pydub import AudioSegment
 
 
-# TODO : CHECK WHAT HAPPENS WHEN FFMPEG NOT PRESENT
-# TODO : USE DIRECTORY AS ARGUMENT
+# Directory to store edited files
+DIRECTORY = 'NORMALIZED'
 
-DIRECTORY = '_NORMALIZED'  # Directory to store edited files
+if not os.path.exists(DIRECTORY):  # Create the directory if it doesn't exist
+    os.mkdir(DIRECTORY)
+
+start = datetime.now()
+
+Supported = ('.wav', '.flac', '.mp3', '.ogg', '.webm', '.mp4')
 
 
 # Check if given files exist in the file system
@@ -21,14 +26,12 @@ def valid_files(arg):
     if not os.path.isfile(arg):
         msg = 'File cannot be found : {}'.format(arg)
         raise argparse.ArgumentTypeError(msg)
+    elif not arg.endswith(Supported):
+        msg = 'File format is not supported : {}'.format(arg)
+        raise argparse.ArgumentTypeError(msg)
     else:
         return arg
 
-
-start = datetime.now()
-
-if not os.path.exists(DIRECTORY):  # Create the directory if it doesn't exist
-    os.mkdir(DIRECTORY)
 
 parser = argparse.ArgumentParser(description="""Normalize audio files to a specified level of dBs
     (supports .mp3 and .flac) \nSupported audio formats : .wav, .flac, .mp3, .ogg, .webm, .mp4""")
@@ -42,11 +45,8 @@ args = parser.parse_args()
 target_dbfs = args.decibels
 if args.all:  # User selected --all
     Audio = []
-    Supported = ('.wav', '.flac', '.mp3', '.ogg', '.webm', '.mp4')
-    # TODO : SUPPORT MORE FORMATS (ERROR IMPORTING .WMA - ERROR EXPORTING .M4A, .APE, .AAC)
-
     for f in os.listdir():
-        if f.endswith(Supported):  # +++++++++++++
+        if f.endswith(Supported):
             Audio.append(f)
     if not Audio:
         print('Current directory has no audio files.')
@@ -54,24 +54,31 @@ if args.all:  # User selected --all
 else:  # User selected --files
     Audio = args.files
 
+show_message = True
 for file in Audio:
-
     (dirname, filename) = os.path.split(file)
     (shortname, extension) = os.path.splitext(filename)
 
-    tags = mutagen.File(file, easy=True)  # Extract metadata information
-
+    # Extract metadata information
+    tags = mutagen.File(file, easy=True)
     # Export the edited file
-    song = AudioSegment.from_file(file, format=extension[1:])
+    try:
+        song = AudioSegment.from_file(file, format=extension[1:])
+    except FileNotFoundError:  # Could not locate FFmpeg
+        if show_message:
+            print('WARNING : Could not locate FFmpeg, skipping non-wav files.')
+            show_message = False
+        continue
     change_in_dBFS = target_dbfs - song.dBFS
     dest_file = os.path.join(DIRECTORY, filename)
     normalized_sound = song.apply_gain(change_in_dBFS)
     bitrate = '320k' if 'mp3' in extension else None
     normalized_sound.export(dest_file, format=extension[1:], bitrate=bitrate)
-
     # Copy metadata to new file (if the original has metadata)
     if tags:
         tags.save(dest_file)
 
-
 print('Execution time : {} seconds'.format((datetime.now() - start).seconds))
+
+# TODO : SUPPORT MORE FORMATS (ERROR IMPORTING .WMA - ERROR EXPORTING .M4A, .APE, .AAC)
+# TODO : REPLACE PRINTS
